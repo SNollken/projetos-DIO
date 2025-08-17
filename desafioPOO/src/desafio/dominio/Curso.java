281a5992aade712e0f1bc92e6e49354a62631be6
Sofia <soso.novaes@gmail.com>
2025-08-17 19:48:33 -0300

    final

--- desafioPOO/src/desafio/dominio/Curso.java
+++ desafioPOO/src/desafio/dominio/Curso.java
@@ -1,24 +1,10 @@
 package desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
 	
 	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -26,9 +12,14 @@ public class Curso {
 		this.cargaHoraria = cargaHoraria;
 	}
 	@Override
-	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
 	}
+	@Override
+	public String toString() {
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + getCargaHoraria() + "]";
+	}
+
 	
 	
 }
--- desafioPOO/src/desafio/dominio/Main.java
+++ desafioPOO/src/desafio/dominio/Main.java
@@ -15,17 +15,51 @@ public class Main {
 		curso2.setDescricao("descricao curso js");
 		curso2.setCargaHoraria(2);
 		
-	
-		
 		
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("mentoria java");
 		mentoria.setDescricao("descricao mentoria java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
+		
+		
+		/*System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria);
+		System.out.println(mentoria);*/
+		
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java");
+		bootcamp.setDescricao("Descricao Bootcamp Java");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devSofia = new Dev();
+		devSofia.setNome("Sofia");
+		devSofia.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Sofia: " + devSofia.getConteudosInscritos());
+		System.out.println("-");
+		devSofia.progredir();
+		devSofia.progredir();
+		System.out.println("Conteudos Inscritos Sofia: " + devSofia.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Sofia: " + devSofia.getConteudosInscritos());
+		System.out.println("XP: " + devSofia.calcularXp());
+
+		System.out.println("-------------------");
+		
+		Dev devJoao = new Dev();
+		devJoao.setNome("Joao");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Joao: " + devJoao.getConteudosInscritos());
+		System.out.println("-");
+		devJoao.progredir();
+		System.out.println("Conteudos Inscritos Joao: " + devJoao.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Joao: " + devJoao.getConteudosInscritos());
+		System.out.println("XP: " + devJoao.calcularXp());
+		
+		
+		
 	}
 
 }
